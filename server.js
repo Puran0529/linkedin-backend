770a11d548574811f886c7588a3bba2ad111a89d
Puranjay Salaria <Puranjay1200@gmail.com>
2025-06-15 02:36:03 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,5 +10,4 @@ app.use(express.json());
 app.use('/api/scrape', scrapeRoute);
 
 const PORT = process.env.PORT || 5000;
-const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
